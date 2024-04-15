c08e2cfd8257098733b31e5411d96c6f389b1d5b
Joy <yzhang817@163.com>
2024-04-15 22:12:57 +0800

    文远知行4.14 第二题

--- src/main/java/ee/exams/wenyuanzhixing4_14/Second.java
+++ src/main/java/ee/exams/wenyuanzhixing4_14/Second.java
@@ -26,30 +26,26 @@ public class Second {
     }
     private static int solution(int N, String s, int M) {
         //使用哈希表统计任务数量
-        int[][] hash = new int[26][1];
+        int[] hash = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            hash[s.charAt(i) - 'A'][0]++;
+            hash[s.charAt(i) - 'A']++;
         }
         //哈希表排序
-        Arrays.sort(hash, (a, b)-> {
-            return a[0] > b[0] ? -1 : 1;
-        });
+        Arrays.sort(hash);
 
         //统计最多任务的类型数量
         int count = 1;
-        for (int i = 1; i < 26; i++) {
-            if (hash[i][0] == hash[i - 1][0]) {
+        for (int i = 24; i >= 0; i--) {
+            if (hash[i] == hash[i + 1]) {
                 count++;
-            } else {
-                break;
-            }
+            } else break;
         }
-        System.out.println(count);
+        //System.out.println(count);
         //同一类型任务的最大任务数
-        int maxTaskNum = hash[0][0];
-        System.out.println(maxTaskNum);
+        int maxTaskNum = hash[25];
+        //System.out.println(maxTaskNum);
         int base = (maxTaskNum - 1) * (M + 1);
-        System.out.println(base);
+        //System.out.println(base);
         int left = N <= (base + count) ? 0 : (N - base - count);
         return base + count + left;
     }
